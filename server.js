38b4fa9bba474fca74d5a7664338854fb1ffe2e2
uriel87 <urielch87@gmail.com>
2016-07-15 02:14:54 +0300

    fix port

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use('/js', express.static(__dirname + '/js'));
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/views', express.static(__dirname + '/views'));
 
-app.set('port', process.env.PORT || 3000);
-app.use('/', express.static('./'));
+// app.set('port', process.env.PORT || 3000);
+// app.use('/', express.static('./'));
 
-// app.use('/', express.static('./')).listen(8000);
+app.use('/', express.static('./')).listen(process.env.PORT || 3000);
 console.log('Client server listening on port 8000 ..\n');
